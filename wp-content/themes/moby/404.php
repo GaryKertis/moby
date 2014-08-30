7b0cd55a2df315d78839d7f9ebb42aa2ad7b33fb
Gary Kertis <gary.kertis@gmail.com>
2014-08-30 16:41:16 -0400

    Fix 404 and remove unwanted files

--- wp-content/themes/moby/404.php
+++ wp-content/themes/moby/404.php
@@ -4,35 +4,22 @@
 
 				<div id="inner-content" class="wrap cf">
 
-					<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+					<div id="main" class="m-all cf" role="main">
 
-						<article id="post-not-found" class="hentry cf">
+						<div id="post" class="mywaves">
+							<canvas id="waves" class="waves" width="100%" height="100px"></canvas>
+							<div class="post_wrapper">
+							<article id="post" <?php post_class( 'cf' ); ?> role="article">
 
-							<header class="article-header">
+								<h1 class="h1 entry-title">404</h1>
+								
 
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
-
-							</header>
-
-							<section class="entry-content">
-
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
-
-							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
-
-							</footer>
-
-						</article>
+							</article>
+							<div id="whale-<?php the_ID(); ?>" class="whale">
+							
+					</div>
+						</div>
+						</div>
 
 					</div>
 
